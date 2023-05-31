02401453c7c55074928d27de3a6ab14890925bd5
Mohit Fating <77158042+MohitFating@users.noreply.github.com>
2023-05-31 23:37:42 +0530

    Create ArrayExe1.java

---
+++ ArrayExe1.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class Exp1 {
+
+	public static void main(String[] args) {
+		
+		int[] data = new int[4];
+		
+		data[0]=45;
+		data[1]=25;
+		data[2]=35;
+		data[3]=55;
+		
+		for(int i =0;i<data.length;i++)
+		{
+			
+		System.out.println("Given array is " + data[i]);
+		
+		}
+	}
+
+}
